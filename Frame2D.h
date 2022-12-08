865ef1974a5e63e0c4b5b340a68ba635e206da17
Tomek Zebrowski <zebromek@gmail.com>
2022-12-08 23:59:40 +0100

    added comments

--- Frame2D.h
+++ Frame2D.h
@@ -2,8 +2,8 @@
 #define FRAME_H
 #include "Vector2D.h"
 
-// Frame2D class represents a frame of reference in 2D space.
-// It can be used to convert between local and global coordinates.
+/// @brief A class representing a 2D frame.
+/// @details This class is used to represent a 2D frame. It has a position, rotation, and scale, and it has a number of functions to perform operations on the frame.
 class Frame2D {
     private:
         Frame2D* parent;
@@ -11,6 +11,8 @@ class Frame2D {
         Vector2D scale;
         float rotation;
     public:
+        /// @brief Creates a new default frame.
+        /// @details This function creates a new default frame. It sets the parent to NULL, the position to (0, 0), the rotation to 0, and the scale to (1, 1).
         Frame2D() {
             parent = NULL;
             position = Vector2D(0, 0);
@@ -18,6 +20,12 @@ class Frame2D {
             rotation = 0;
         }
 
+        /// @brief Creates a new frame with the specified parent.
+        /// @param parent The parent of the frame.
+        /// @param position The position of the frame.
+        /// @param rotation The rotation of the frame.
+        /// @param scale The scale of the frame.
+        /// @details This function creates a new frame with the specified parent. It sets the position, rotation, and scale to the specified values.
         Frame2D(Frame2D* parent, Vector2D position, float rotation, Vector2D scale) {
             this->parent = parent;
             this->position = position;
@@ -25,7 +33,11 @@ class Frame2D {
             this->scale = scale;
         }
 
-        // Converts a vector from global coordinates to local coordinates. 
+        /// @brief Converts a vector from global coordinates to local coordinates.
+        /// @param globalVector The vector to convert.
+        /// @return The converted vector.
+        /// @details This function converts a vector from global coordinates to local coordinates. It does this by first converting the vector to the parent's local coordinates, then subtracting the position, rotating by the negative rotation, and scaling by the inverse scale.
+        /// @note This function is recursive, and will call itself until it reaches the root frame. 
         Vector2D getLocalCoordinates(Vector2D globalVector) {
             if (parent == NULL) {
                 return (globalVector - position).rotate(-rotation) / scale;
@@ -33,7 +45,11 @@ class Frame2D {
             return (parent->getLocalCoordinates(globalVector) - position).rotate(-rotation) / scale;
         }
 
-        // Converts a vector from local coordinates to global coordinates.
+        /// @brief Converts a vector from local coordinates to global coordinates.
+        /// @param localVector The vector to convert.
+        /// @return The converted vector.
+        /// @details This function converts a vector from local coordinates to global coordinates. It does this by first scaling, rotating, and adding the position, then converting the vector to the parent's global coordinates.
+        /// @note This function is recursive, and will call itself until it reaches the root frame.
         Vector2D getGlobalCoordinates(Vector2D localVector) {
             if (parent == NULL) {
                 return (localVector * scale).rotate(rotation) + position;
@@ -41,34 +57,50 @@ class Frame2D {
             return parent->getGlobalCoordinates((localVector * scale).rotate(rotation) + position);
         }
 
+        /// @brief Sets the parent of the frame.
+        /// @param parent The new parent of the frame.
         void setParent(Frame2D* parent) {
             this->parent = parent;
         }
 
+        /// @brief Sets the position of the frame.
+        /// @param position The new position of the frame.
         void setPosition(Vector2D position) {
             this->position = position;
         }
 
+        /// @brief Sets the rotation of the frame.
+        /// @param rotation The new rotation of the frame.
         void setRotation(float rotation) {
             this->rotation = rotation;
         }
 
+        /// @brief Sets the scale of the frame.
+        /// @param scale The new scale of the frame.
         void setScale(Vector2D scale) {
             this->scale = scale;
         }
 
+        /// @brief Gets the parent of the frame.
+        /// @return The parent of the frame.
         Frame2D* getParent() {
             return parent;
         }
 
+        /// @brief Gets the position of the frame.
+        /// @return The position of the frame.
         Vector2D getPosition() {
             return position;
         }
 
+        /// @brief Gets the rotation of the frame.
+        /// @return The rotation of the frame.
         float getRotation() {
             return rotation;
         }
 
+        /// @brief Gets the scale of the frame.
+        /// @return The scale of the frame.
         Vector2D getScale() {
             return scale;
         }
--- Vector2D.h
+++ Vector2D.h
@@ -3,16 +3,25 @@
 #include <iostream>
 #include <cmath>
 
+/// @brief A class representing a 2D vector.
+/// @details This class is used to represent a 2D vector. It has an x and y component, and it has a number of functions to perform operations on the vector.
 class Vector2D {
     public:
+        /// @brief The x component of the vector.
         float x;
+        /// @brief The y component of the vector.
         float y;
 
+        /// @brief Creates a new vector with the specified x and y components.
+        /// @param x The x component of the vector.
+        /// @param y The y component of the vector. 
         Vector2D(float x, float y) {
             this->x = x;
             this->y = y;
         }
 
+        /// @brief Creates a new zero vector.
+        /// @details This function creates a new zero vector. It sets the x and y components to 0.
         Vector2D() {
             this->x = 0;
             this->y = 0;
@@ -70,23 +79,42 @@ class Vector2D {
             return *this;
         }
 
+        /// @brief Get the magnitude of the vector.
+        /// @return The magnitude of the vector.
+        /// @details This function returns the magnitude of the vector. The magnitude is the length of the vector.
+        /// @see sqrMagnitude()
         float magnitude() const {
             return std::sqrt(x * x + y * y);
         }
 
+        /// @brief Get the squared magnitude of the vector.
+        /// @return The squared magnitude of the vector.
+        /// @details This function returns the squared magnitude of the vector. The squared magnitude is the length of the vector squared.
+        /// @see magnitude()
+        /// @note This function is faster than magnitude(), and should be used if you only need to compare the magnitude of two vectors.
         float sqrMagnitude() const {
             return x * x + y * y;
         }
 
+        /// @brief Get the normalized vector.
+        /// @return The normalized vector.
+        /// @details This function returns the normalized vector. The normalized vector is the vector with the same direction as the original vector, but with a magnitude of 1.
         Vector2D normalized() const {
             float m = magnitude();
             return Vector2D(x / m, y / m);
         }
 
+        /// @brief Get the perpendicular vector.
+        /// @return The perpendicular vector.
+        /// @details This function returns the perpendicular vector. The perpendicular vector is the vector with the same magnitude as the original vector, but with a direction 90 degrees to the right of the original vector.
         Vector2D perpendicular() const {
             return Vector2D(-y, x);
         }
 
+        /// @brief Get the angle between two vectors.
+        /// @param v The vector to compare the angle to.
+        /// @return The angle between the two vectors.
+        /// @details This function returns the angle between two vectors. The angle is in radians.
         float angle(const Vector2D& v=Vector2D::right()) const {
             float a = atan2(cross(v), dot(v));
             if (a < 0) {
@@ -95,6 +123,11 @@ class Vector2D {
             return a;
         }
 
+        /// @brief Rotate the vector.
+        /// @param angle The angle to rotate the vector by.
+        /// @return The rotated vector.
+        /// @details This function returns the rotated vector. The angle is in radians.
+        /// @see angle()
         Vector2D rotate(float angle) const{
             float s = sin(angle);
             float c = cos(angle);
@@ -115,14 +148,25 @@ class Vector2D {
             return *this;
         }
 
+        /// @brief Get the dot product of two vectors.
+        /// @param v The vector to dot with.
+        /// @return The dot product of the two vectors.
+        /// @details This function returns the dot product of two vectors. The dot product is the sum of the products of the components of the vectors.
         float dot(const Vector2D& v) const {
             return x * v.x + y * v.y;
         }
 
+        /// @brief Get the cross product of two vectors.
+        /// @param v The vector to cross with.
+        /// @return The cross product of the two vectors.
+        /// @details This function returns the cross product of two vectors. The cross product is the magnitude of the vector perpendicular to the two vectors.
         float cross(const Vector2D& v) const {
             return x * v.y - y * v.x;
         }
         
+        /// @brief Get the distance between two vectors.
+        /// @param max The maximum magnitude of the vector.
+        /// @return The vector with a magnitude of max if the original vector is greater than max, otherwise the original vector.
         Vector2D clampMagnitude(float max) const {
             if (sqrMagnitude() > max * max) {
                 return normalized() * max;
@@ -135,34 +179,61 @@ class Vector2D {
             return os;
         }
         
+        /// @brief Get the zero vector.
+        /// @return A vector (0, 0).
         static Vector2D zero() {
             return Vector2D(0, 0);
         }
 
+        /// @brief Get the one vector.
+        /// @return A vector (1, 1).
         static Vector2D one() {
             return Vector2D(1, 1);
         }
 
+        /// @brief Get the up vector.
+        /// @return A vector (0, 1).
         static Vector2D up() {
             return Vector2D(0, 1);
         }
 
+        /// @brief Get the down vector.
+        /// @return A vector (0, -1).
         static Vector2D down() {
             return Vector2D(0, -1);
         }
 
+        /// @brief Get the left vector.
+        /// @return A vector (-1, 0).
         static Vector2D left() {
             return Vector2D(-1, 0);
         }
 
+        /// @brief Get the right vector.
+        /// @return A vector (1, 0).
         static Vector2D right() {
             return Vector2D(1, 0);
         }
 
+        /// @brief Interpolate between two vectors.
+        /// @param v1 The first vector.
+        /// @param v2 The second vector.
+        /// @param t The interpolation factor.
+        /// @return The interpolated vector.
+        /// @details This function returns the interpolated vector. The interpolation factor is a value between 0 and 1. If t is 0, the interpolated vector is v1. If t is 1, the interpolated vector is v2. If t is 0.5, the interpolated vector is halfway between v1 and v2.
+        /// @see slerp()
         static Vector2D lerp(const Vector2D& v1, const Vector2D& v2, float t) {
             return v1 + (v2 - v1) * t;
         }
 
+        /// @brief Interpolate between two vectors using spherical linear interpolation.
+        /// @param v1 The first vector.
+        /// @param v2 The second vector.
+        /// @param t The interpolation factor.
+        /// @return The interpolated vector.
+        /// @details This function returns the interpolated vector. The interpolation factor is a value between 0 and 1. If t is 0, the interpolated vector is v1. If t is 1, the interpolated vector is v2. If t is 0.5, the interpolated vector is halfway between v1 and v2.
+        /// @see lerp()
+        /// @note This function uses spherical linear interpolation, which is more accurate than linear interpolation, but is more expensive to compute. Use lerp() if you don't need the accuracy.
         static Vector2D slerp(const Vector2D& v1, const Vector2D& v2, float t) {
             float angle = v1.angle(v2);
             return (v1 * sin(angle * (1 - t)) + v2 * sin(angle * t)) / sin(angle);
